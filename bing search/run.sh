a6c5a393b826a0bd389feb5e29ec130f95c02ce8
Guanqun Mao <guanqunm@gmail.com>
2014-06-16 18:35:25 -0400

    modify bash

--- bing search/run.sh
+++ bing search/run.sh
@@ -1,4 +1,4 @@
 rm -f Bing.class
 javac -cp .:commons-codec-1.9.jar:json-simple-1.1.jar  Bing.java
 #echo "$@"
-java -cp .:commons-codec-1.9.jar:json-simple-1.1.jar  Bing "$@"
+java -cp .:commons-codec-1.9.jar:json-simple-1.1.jar  Bing "$@"  > test.json
