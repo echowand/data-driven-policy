7721e8c4c695ef79b79f50a632766cb80a1da81f
Guanqun Mao <guanqunm@gmail.com>
2014-06-16 21:02:40 -0400

    add readme

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-2014 6/3
+2014/6/3
 This repository was created by Guanqun Mao, Xinpi Du, and Weijie Huo @Grey House
 
----------------------------------------------------------------
+-------------------------------------------------------------------
+
+
+## Preprocess Procedure
+In bing-search directory, run: 
+	./run.sh KEYWORD
+Open the browser and go to:
+	http://localhost:8983/solr/#/collection1/query?q=KEYWORD
+All the relevant queries will be displayed. 
+
+
+## GUI
+
+
+Apache SOLR
+
+ 
+
+	
+## NLP
 
 
---
+++ bing-search/Bing.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//package bing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.codec.binary.Base64;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.*;
+/**
+ *
+ * @author pumpkin
+ */
+public class Bing {
+    
+    /**
+     * 
+     * @param query - query to use for the search
+     * @return - a json array of results. each contains a title, description, url, 
+     * and some other metadata that can be easily extracted since its in json format
+     */
+    public static JSONArray search(String query){
+        try {
+            query = "'"+query+"'";
+            String encodedQuery = URLEncoder.encode(query, "UTF-8");
+            //System.out.println(encodedQuery);
+//            URL url = new URL("https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27web%27&Query=%27car%27");
+//            URL url = new URL("http://csb.stanford.edu/class/public/pages/sykes_webdesign/05_simple.html");
+            String webPage = "https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27web%27&Query="+encodedQuery+"&$format=JSON";
+            String name = "6604d12c-3e89-4859-8013-3132f78c1595";
+            String password = "cefgNRl3OL4PrJJvssxkqLw0VKfYNCgyTe8wNXotUmQ";
+
+            String authString = name + ":" + password;
+            //System.out.println("auth string: " + authString);
+            byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+            String authStringEnc = new String(authEncBytes);
+            //System.out.println("Base64 encoded auth string: " + authStringEnc);
+
+            URL url = new URL(webPage);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine).append("\n");
+            }
+            
+            in.close();
+            JSONParser parser = new JSONParser();
+            JSONArray arr = (JSONArray)((JSONObject)((JSONObject)parser.parse(response.toString())).get("d")).get("results");
+            JSONObject obj = (JSONObject)arr.get(0);
+            JSONArray out = (JSONArray)obj.get("Web");
+            
+            return out;
+            
+//            
+            
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Bing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Bing.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(Bing.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    
+    
+    
+    
+    
+    public static void main (String[] args){
+        
+        String search = new String();
+        if (args.length == 1) {
+            try {
+                search = new String(args[0]);
+                search = search.replace("\"", "");
+            } catch (NumberFormatException e) {
+               
+            }
+        }
+
+        JSONArray results = Bing.search(search);
+        //iterate through the results, printing out each entry.
+        JSONArray newRes = new JSONArray();
+        int i = 0;
+        for(Object res : results){
+            JSONObject obj = (JSONObject)res;
+            String url = (String) obj.get("Url");
+            //System.out.println(search+" "+url);
+            JSONObject newObj = new JSONObject();
+            i++;
+            newObj.put("id",i);
+            newObj.put("name", search);
+            newObj.put("url",url);
+            newRes.add(newObj);
+        }
+        //System.out.println("{");
+        StringWriter out = new StringWriter();
+        try{
+            newRes.writeJSONString(out);
+        }
+        catch(Exception e){}
+        String jsontxt = out.toString();
+        System.out.print(jsontxt);
+        i = 0;
+        for(Object res : newRes){
+            JSONObject obj = (JSONObject)res;
+            //if(i == newRes.size()-1)System.out.println(obj);
+            //else System.out.println(obj+",");
+            i++;
+        }
+        //System.out.println(res.toJSONString());
+        //System.out.println("}");
+        
+    }
+    
+}
