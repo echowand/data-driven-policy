c11687526b5905370cf7904999ab8dfb4fb9400c
Guanqun Mao <guanqunm@gmail.com>
2014-06-16 21:06:18 -0400

    update bing

--- bing search/run.sh
+++
@@ -1,6 +0,0 @@
-rm -f Bing.class
-javac -cp .:commons-codec-1.9.jar:json-simple-1.1.jar  Bing.java
-#echo "$@"
-rm -f test.json
-java -cp .:commons-codec-1.9.jar:json-simple-1.1.jar  Bing "$@"  > test.json
-java -Dtype=application/json -jar post.jar test.json
