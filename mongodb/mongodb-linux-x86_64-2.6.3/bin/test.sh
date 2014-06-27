55f6cb4051942bdeda6f480294709299e47dd4a4
duxinpi <du25@moore02.cs.purdue.edu>
2014-06-26 23:30:16 -0400

    mongodb uploaded, including ReadMe file in /mongodb/mongodb-linux-x86_64-/2.6.3/bin, which can help to get a quick start.

---
+++ mongodb/mongodb-linux-x86_64-2.6.3/bin/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH=/homes/du25/scratch/summer_research/mongodb/mongodb-linux-x86_64-2.6.3/bin:$PATH
+mongod --dbpath /homes/du25/scratch/summer_research/mongodb/mongodb-linux-x86_64-2.6.3/data/db
