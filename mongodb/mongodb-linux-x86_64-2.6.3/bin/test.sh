dd314cb798eda61acdf8372384fd457ecc8d6eae
duxinpi <du25@moore02.cs.purdue.edu>
2014-06-27 00:02:56 -0400

    Dont forget modify test.sh file if changed data store directory

--- mongodb/mongodb-linux-x86_64-2.6.3/bin/test.sh
+++ mongodb/mongodb-linux-x86_64-2.6.3/bin/test.sh
@@ -1,3 +1,4 @@
+#Before running test.sh, open terminal and input "chmod +x test.sh" to get permission of running bash file.
 #!/bin/bash
-export PATH=/homes/du25/scratch/summer_research/mongodb/mongodb-linux-x86_64-2.6.3/bin:$PATH
-mongod --dbpath /homes/du25/scratch/summer_research/mongodb/mongodb-linux-x86_64-2.6.3/data/db
+export PATH=/homes/du25/scratch/summer_research/data-driven-policy/mongodb/mongodb-linux-x86_64-2.6.3/bin:$PATH
+mongod --dbpath /homes/du25/scratch/summer_research/data-driven-policy/mongodb/mongodb-linux-x86_64-2.6.3/data/db
