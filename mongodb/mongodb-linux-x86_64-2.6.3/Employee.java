5a1f8a1b07458ab0b49d60cd88c6ec26660b64b5
duxinpi <du25@pod5-5.cs.purdue.edu>
2014-06-28 20:54:04 -0400

    I uploaded two test case for java parsing json, 1. Parse_json.java&Test_json.java, it can import json file to java. 2.Employee.java&JsonHelper.java&ParseJson.java, it can convert back and forth among json-map-javabean.

---
+++ mongodb/mongodb-linux-x86_64-2.6.3/Employee.java
@@ -0,0 +1,29 @@
+
+public class Employee {
+	
+     String name;
+     String sex;
+     int age;
+     public void setName(String s){
+    	 this.name=s;
+    	 
+    	 
+     }
+     public void setSex(String s){
+    	 
+    	 this.sex=s;
+     }
+     public void setAge(int s){
+    	 this.age=s;
+    	 
+     }
+     public String getName(){
+    	 return this.name;
+     }
+     public String getSex(){
+    	 return this.name;
+     }
+     public String getAge(){
+    	 return this.name;
+     }
+}
---
+++ mongodb/mongodb-linux-x86_64-2.6.3/Parse_json.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class Parse_json {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+String JsonContext = new Test_json().ReadFile("/homes/du25/scratch/summer_research/data-driven-policy/solr-4.8.1/example/exampledocs/sk.json");
+JSONArray jsonArray = JSONArray.fromObject(JsonContext);
+int size = jsonArray.size();
+System.out.println("Size: " + size);
+for(int  i = 0; i < size; i++){
+JSONObject jsonObject = jsonArray.getJSONObject(i);
+System.out.println("[" + i + "]name=" + jsonObject.get("name"));
+System.out.println("[" + i + "]package_name=" + jsonObject.get("package_name"));
+System.out.println("[" + i + "]check_version=" + jsonObject.get("check_version"));
+}
+}
+
+}
